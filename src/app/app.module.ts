675c2bf20f51a6defe21436ff835f5a59314bda1
encount <admin@encounter.co.nz>
2017-09-13 21:12:17 +1200

    generate file-drop for drag and drop user avatar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FileDropDirective } from './shared/directives/file-drop.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileDropDirective
   ],
   imports: [
     BrowserModule,
