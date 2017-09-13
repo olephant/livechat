72029a0a8cf189fd30876fb04180a37c91a5480b
encount <admin@encounter.co.nz>
2017-09-13 23:22:51 +1200

    chat form logic

--- src/app/chat-room/chat-form/chat-form.component.ts
+++ src/app/chat-room/chat-form/chat-form.component.ts
@@ -1,3 +1,7 @@
+import { MessageService } from './../../shared/services/message.service';
+import { UsersService } from './../../shared/services/users.service';
+import { Subscription } from 'rxjs/Rx';
+import { Message } from './../../shared/models/message';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatFormComponent implements OnInit {
 
-  constructor() { }
+  message: Message = new Message();
+
+  subscription: Subscription;
+
+  email: string;
+  name: string;
+  avatar: string;
+
+  constructor(
+    private usersService: UsersService,
+    private messageService: MessageService) { }
 
   ngOnInit() {
+    // get current users details so we can add to message
+    this.subscription = this.usersService.userDetails.subscribe(
+      (profile) => {
+        this.email = profile.email;
+        this.name = profile.name;
+        this.avatar = profile.avatar;
+      }
+    );
+  }
+
+  sendMessage() {
+    // no input
+    if (this.message.body === undefined) {
+      return;
+    }
+    this.message.email = this.email;
+    this.message.name = this.name;
+    this.message.avatar = this.avatar;
+    this.messageService.createItem(this.message);
+    this.message = new Message();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
