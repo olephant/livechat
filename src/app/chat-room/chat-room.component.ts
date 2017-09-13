415b815fbda93203ff47b394ea84071beacaa974
encount <admin@encounter.co.nz>
2017-09-13 23:16:00 +1200

    get current user and add welcome to display

--- src/app/chat-room/chat-room.component.ts
+++ src/app/chat-room/chat-room.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { UsersService } from './../shared/services/users.service';
+import { AuthService } from './../shared/services/auth.service';
+import { Subscription } from 'rxjs/Rx';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.scss']
 })
-export class ChatRoomComponent implements OnInit {
+export class ChatRoomComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  subscription: Subscription;
+  welcomeMessage: string;
+
+  constructor(
+    private auth: AuthService,
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
+    // current users profile add to subject so we can access it everywhere
+    this.subscription = this.usersService.getUser(this.auth.currentUserId).subscribe(
+      (user) => {
+        this.welcomeMessage = 'Welcome ' + user.name;
+        this.usersService.userDetails.next(user);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    // remove message when logged out
+    this.welcomeMessage = '';
   }
 
 }
