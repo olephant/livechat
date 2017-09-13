051a2ad06432433f59cab97737ac2f1328a7d3d7
encount <admin@encounter.co.nz>
2017-09-13 23:23:06 +1200

    create a new message

--- src/app/shared/services/message.service.ts
+++ src/app/shared/services/message.service.ts
@@ -1,8 +1,26 @@
+import { Subject } from 'rxjs/Rx';
+import { Message } from './../models/message';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MessageService {
 
-  constructor() { }
+  // let components know when new message is created
+  // will allow us to set scroll height
+  newMessage = new Subject<any>();
+
+  items: FirebaseListObservable<Message[]> = null;
+  item: FirebaseObjectObservable<Message> = null;
+
+  constructor(
+    private ngDb: AngularFireDatabase
+  ) { }
+
+  createItem(item: Message): void {
+    this.items.push(item)
+      .then(() => this.newMessage.next('new message'))
+      .catch(error => console.log(error.message, 'creating message'));
+  }
 
 }
