2c8679fac84692c47af63c01de42ed82551c2afa
encount <admin@encounter.co.nz>
2017-09-13 21:36:07 +1200

    login markup and style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms/src/directives';
+import { AuthService } from './../shared/services/auth.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
+
+  onSubmit(form: NgForm) { }
+
+  handleErrors(errorMsg) { }
+
+  ngOnDestroy(): void { }
 
 }
