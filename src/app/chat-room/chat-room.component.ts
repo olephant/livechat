ff6f26159613cc3fb44b718d520c747a3bea81a4
encount <admin@encounter.co.nz>
2017-09-13 23:41:04 +1200

    add scroller

--- src/app/chat-room/chat-room.component.ts
+++ src/app/chat-room/chat-room.component.ts
@@ -1,21 +1,25 @@
+import { MessageService } from './../shared/services/message.service';
 import { UsersService } from './../shared/services/users.service';
 import { AuthService } from './../shared/services/auth.service';
 import { Subscription } from 'rxjs/Rx';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.scss']
 })
-export class ChatRoomComponent implements OnInit, OnDestroy{
+export class ChatRoomComponent implements OnInit, OnDestroy, AfterContentInit{
 
   subscription: Subscription;
   welcomeMessage: string;
 
+  @ViewChild('scroller') private feedContainer: ElementRef;
+
   constructor(
     private auth: AuthService,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private messageService: MessageService
   ) { }
 
   ngOnInit() {
@@ -26,6 +30,26 @@ export class ChatRoomComponent implements OnInit, OnDestroy{
         this.usersService.userDetails.next(user);
       }
     );
+
+    this.messageService.newMessage.subscribe(
+      (msg) =>  {
+        setTimeout( () => {
+          this.scroller();
+        }, 1  );
+      }
+    );
+  }
+
+  ngAfterContentInit() {
+    this.messageService.newMessage.subscribe(
+      (msg) =>  this.scroller()
+    );
+  }
+
+
+  scroller() {
+    this.feedContainer.nativeElement.scrollTop
+    = this.feedContainer.nativeElement.scrollHeight;
   }
 
   ngOnDestroy(): void {
