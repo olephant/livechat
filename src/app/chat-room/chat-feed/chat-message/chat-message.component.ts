896e50bc13657d694c70d6e8d4f82cb1690d6671
encount <admin@encounter.co.nz>
2017-09-13 23:34:28 +1200

    style a new message depending on sender

--- src/app/chat-room/chat-feed/chat-message/chat-message.component.ts
+++ src/app/chat-room/chat-feed/chat-message/chat-message.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Rx';
+import { Message } from './../../../shared/models/message';
+import { AuthService } from './../../../shared/services/auth.service';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-chat-message',
   templateUrl: './chat-message.component.html',
   styleUrls: ['./chat-message.component.scss']
 })
-export class ChatMessageComponent implements OnInit {
+export class ChatMessageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input() message: Message;
 
-  ngOnInit() {
+  subscription: Subscription;
+
+  currentEmail: string;
+  userEmail: string;
+  ownMessage: boolean;
+
+  user: Observable<firebase.User>;
+
+  constructor(
+    private auth: AuthService
+  ) {
+      this.user = auth.currentUserObservable.take(1);
+      this.subscription = this.user.subscribe(
+        (value) => {
+          this.userEmail = value.email;
+          this.ownMessage = this.userEmail === this.currentEmail;
+      });
+   }
+
+  ngOnInit(message = this.message) {
+    this.currentEmail = message.email;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
