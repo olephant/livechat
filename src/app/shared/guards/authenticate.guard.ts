8435b77e750506ca52a4c7f8d8d75a89b09b702d
encount <admin@encounter.co.nz>
2017-09-13 21:28:03 +1200

    add authenticate guard logic to redirect to login page

--- src/app/shared/guards/authenticate.guard.ts
+++ src/app/shared/guards/authenticate.guard.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot,
+         CanActivate,
+         Router,
+         RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
 
 @Injectable()
 export class AuthenticateGuard implements CanActivate {
+
+  user: Observable<firebase.User>;
+
+  constructor(private afAuth: AngularFireAuth,
+              private router: Router) {
+                this.user = afAuth.authState;
+                }
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot):
+      Observable<boolean> | Promise<boolean> | boolean {
+
+      return this.user.map(
+        (auth) => {
+          if (!auth) {
+            this.router.navigateByUrl('/login');
+            return false;
+          }
+            return true;
+        }).take(1);
   }
 }
+
+
+
