9f50a0caeb728e88c9e64b5648535a2202286c1f
encount <admin@encounter.co.nz>
2017-09-13 22:38:02 +1200

    login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { Subscription } from 'rxjs/Rx';
 import { NgForm } from '@angular/forms/src/directives';
 import { AuthService } from './../shared/services/auth.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
@@ -9,15 +10,34 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
+  subscription: Subscription;
+
+    haveErrors: boolean;
+    error: string;
+    spinner: boolean;
+
   constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    this.subscription = this.auth.firebaseError.subscribe(
+      (errorMsg) => this.handleErrors(errorMsg)
+    );
   }
 
-  onSubmit(form: NgForm) { }
+  onSubmit(form: NgForm) {
+    this.haveErrors = false;
+    this.spinner = true;
+    this.auth.emailLogin(form.value.email, form.value.password);
+   }
 
-  handleErrors(errorMsg) { }
+  handleErrors(errorMsg) {
+    this.haveErrors = true;
+    this.error = errorMsg;
+    this.spinner = false;
+   }
 
-  ngOnDestroy(): void { }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+   }
 
 }
