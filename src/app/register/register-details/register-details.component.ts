f8a860aa8add818eaf5eec302ba314f53a5d310a
encount <admin@encounter.co.nz>
2017-09-13 22:14:10 +1200

    register details logic

--- src/app/register/register-details/register-details.component.ts
+++ src/app/register/register-details/register-details.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { CropperSettings, ImageCropperComponent } from 'ng2-img-cropper';
+import { AuthService } from './../../shared/services/auth.service';
+import { UsersService } from './../../shared/services/users.service';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-register-details',
   templateUrl: './register-details.component.html',
   styleUrls: ['./register-details.component.scss']
 })
-export class RegisterDetailsComponent implements OnInit {
+export class RegisterDetailsComponent {
 
-  constructor() { }
+  // required for cropping image
+  data: any;
+  @ViewChild('cropper', undefined)
+  cropper: ImageCropperComponent;
+  cropperSettings: CropperSettings;
 
-  ngOnInit() {
+  dragOver = false;
+
+  username: string;
+  hasSelected = false;
+  usernameAvailable = true;
+  hideAvailability: boolean;
+
+  constructor(
+    private auth: AuthService,
+    private UsersService: UsersService
+  ) {
+    this.cropperSettings = new CropperSettings();
+    this.cropperSettings.croppedWidth = 150;
+    this.cropperSettings.croppedHeight = 150;
+    this.cropperSettings.noFileInput = true;
+    this.cropperSettings.rounded = true;
+    this.data = {};
+   }
+
+  dropOverState($event: boolean) {
+    this.dragOver = $event;
   }
+
+   // called when uploaded via click event
+  fileChangeListener($event) {
+    const image: any = new Image();
+    const file: File = $event.target.files[0];
+    const myReader: FileReader = new FileReader();
+    const that = this;
+    myReader.onloadend = function (loadEvent: any) {
+      image.src = loadEvent.target.result;
+      that.cropper.setImage(image);
+    };
+    myReader.readAsDataURL(file);
+    this.hasSelected = true;
+  }
+
+  // called when image dragged
+  fileChangeListenerDrop(fileList: FileList) {
+    const image: any = new Image();
+    const file: File = fileList[0];
+    const myReader: FileReader = new FileReader();
+    const that = this;
+    myReader.onloadend = function (loadEvent: any) {
+      image.src = loadEvent.target.result;
+      that.cropper.setImage(image);
+    };
+    myReader.readAsDataURL(file);
+    this.hasSelected = true;
+  }
+
+  updateProfile() {
+    this.hideAvailability = true;
+    const croppedImage = this.data.image.split(/,(.+)/)[1];
+    this.UsersService.updateProfile(this.auth.currentUserId, this.username, croppedImage);
+  }
+
+  checkUsername() {
+    this.UsersService.checkUsername(this.username).subscribe(
+      (name) => this.usernameAvailable = !name.$value
+    );
+  }
+
 
 }
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,8 +1,48 @@
+import { Router } from '@angular/router';
+import { User } from './../models/user';
+import { FirebaseListObservable, FirebaseObjectObservable, AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UsersService {
 
-  constructor() { }
+  user: FirebaseObjectObservable<User> = null;
+  users: FirebaseListObservable<User[]> = null;
+
+  constructor(
+    private db: AngularFireDatabase,
+    private router: Router) { }
+
+    updateProfile(key: string, username: string, image: string) {
+      username = username.toLowerCase();
+      const path = `users/${key}`;
+      const usernameData = {};
+      usernameData[username] = key;
+      const storageRef = firebase.storage().ref();
+      const uploadTask = storageRef
+        .child(`profile/${key}`)
+        .putString(image, 'base64', {contentType: 'image/png'})
+        .then(
+          (snapshot) => {
+            const updates = {
+            name : username.toLowerCase(),
+            avatar : snapshot.downloadURL
+          };
+
+          this.db.object(path).update(updates)
+            .catch(error => console.log(error));
+          this.db.object(`usernames/`).update(usernameData)
+            .then( () => this.router.navigate(['/chat']))
+            .catch(error => console.log(error));
+          });
+    } // updateProfile
+
+    checkUsername(username: string) {
+      username = username.toLowerCase();
+      return this.db.object(`usernames/${username}`);
+    }
 
 }
+
+
