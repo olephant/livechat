a18a1269d5c478036282782e290e1a12f9a35f7f
encount <admin@encounter.co.nz>
2017-09-13 22:47:50 +1200

    reverse pipe to reorder db so offline users get pushed to bottom

--- src/app/shared/pipes/reverse.pipe.ts
+++ src/app/shared/pipes/reverse.pipe.ts
@@ -5,8 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReversePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
-
+  transform(value) {
+      if (!value) {
+        return;
+      }
+      return value.reverse();
+    }
 }
