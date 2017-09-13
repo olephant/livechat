cd21d7f253426aee40fd64072babcf09b36f0fc7
encount <admin@encounter.co.nz>
2017-09-13 21:58:07 +1200

    add initial registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { AuthService } from './../shared/services/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+  haveErrors: boolean;
+  // error message from firebase
+  error: string;
+  spinner: boolean;
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    // subscribe to subject incase of firebase errors
+    this.subscription = this.auth.firebaseError.subscribe(
+      (errorMessage) => this.handleErrors(errorMessage)
+    );
   }
+
+  onSubmit(form: NgForm) {
+    // if we had previous errors hide them
+    this.haveErrors = false;
+    // display spinner
+    this.spinner = true;
+    this.auth.emailSignUp(form.value.email, form.value.password);
+   }
+
+   handleErrors(errorMessage) {
+     // show error message
+    this.haveErrors = true;
+    // set message
+    this.error = errorMessage;
+    // remove spinner
+    this.spinner = false;
+    }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+   }
 
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,8 +1,75 @@
+import { Subject } from 'rxjs/Rx';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  authState: any = null;
+
+  // track firebase errors that require user display
+  firebaseError = new Subject<string>();
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase,
+    private router: Router) {
+      this.afAuth.authState.subscribe((auth) => {
+      this.authState = auth; });
+    }
+
+  // Returns true if user is logged in
+  get authenticated(): boolean {
+    return this.authState !== null;
+  }
+
+  // Returns current user data
+  get currentUser(): any {
+    return this.authenticated ? this.authState : null;
+  }
+
+  // Returns
+  get currentUserObservable(): any {
+    return this.afAuth.authState;
+  }
+
+  // Returns current user UID
+  get currentUserId(): string {
+    return this.authenticated ? this.authState.uid : '';
+  }
+
+
+  emailSignUp(email: string, password: string): any {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      this.authState = user;
+      this.updateUserData();
+    })
+    .catch(error => this.firebaseError.next(error.message));
+  }
+
+  private updateUserData(): void {
+
+        // Endpoint firebase
+        const path = `users/${this.currentUserId}`;
+
+        // add some default values for name and avitar
+        // incase user does not complete the next section
+        const data = {
+           email: this.authState.email,
+           name: 'default',
+           avatar: 'default', // set it to a default image
+           status: true
+        };
+
+        // after updating user info navigate to the profile page where
+        // user add displayname and avitar
+        this.db.object(path).update(data)
+          .then(() => this.router.navigate(['register', 'profile']))
+          .catch(error => console.log(error.message, ' Add new user error'));
+  } // updateUserData
 
 }
