e322e2d171fddcf885496dbac177d0a1a89cbe7c
encount <admin@encounter.co.nz>
2017-09-13 21:17:21 +1200

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FileDropDirective } from './shared/directives/file-drop.directive';
 import { ReversePipe } from './shared/pipes/reverse.pipe';
+import { RegisterComponent } from './register/register.component';
+import { RegisterDetailsComponent } from './register/register-details/register-details.component';
+import { LoginComponent } from './login/login.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { ChatFeedComponent } from './chat-room/chat-feed/chat-feed.component';
+import { ChatMessageComponent } from './chat-room/chat-feed/chat-message/chat-message.component';
+import { ChatFormComponent } from './chat-room/chat-form/chat-form.component';
+import { ChatUsersComponent } from './chat-room/chat-users/chat-users.component';
+import { ChatUserComponent } from './chat-room/chat-users/chat-user/chat-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FileDropDirective,
-    ReversePipe
+    ReversePipe,
+    RegisterComponent,
+    RegisterDetailsComponent,
+    LoginComponent,
+    ChatRoomComponent,
+    ChatFeedComponent,
+    ChatMessageComponent,
+    ChatFormComponent,
+    ChatUsersComponent,
+    ChatUserComponent
   ],
   imports: [
     BrowserModule,
