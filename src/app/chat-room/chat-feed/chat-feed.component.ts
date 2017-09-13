ae9ab9ddf38fc303759872f4802250a398c73fe2
encount <admin@encounter.co.nz>
2017-09-13 23:34:06 +1200

    get all messages and update new message subject

--- src/app/chat-room/chat-feed/chat-feed.component.ts
+++ src/app/chat-room/chat-feed/chat-feed.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { MessageService } from '../../shared/services/message.service';
+import { Message } from './../../shared/models/message';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat-feed',
   templateUrl: './chat-feed.component.html',
   styleUrls: ['./chat-feed.component.scss']
 })
-export class ChatFeedComponent implements OnInit {
+export class ChatFeedComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  messages: FirebaseListObservable<Message[]>;
+
+  subscription: Subscription;
+
+  constructor(
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
+    this.messages = this.messageService.getItemsList();
+    // force afterinit call for scroller
+    this.subscription = this.messages.subscribe(
+      () => this.messageService.newMessage.next('new mesage')
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
